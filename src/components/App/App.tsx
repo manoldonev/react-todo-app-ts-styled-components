d743cd3954b7199bc97b9abdec0cd937163ca050
Manol Donev <manol.donev@gmail.com>
2021-10-27 15:18:45 +0300

    chore: prettier & eslint extended setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import TodoList from '../TodoList';
+
+function App(): JSX.Element {
+  return <TodoList />;
+}
+
+export default App;
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,3 @@
+export default function TodoList(): JSX.Element {
+  return <div>Todo List</div>;
+}
